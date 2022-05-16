fb21dd20fcc1fd0eae0b2cb8bdaa327bd840633d
amouna89 <95442375+amouna89@users.noreply.github.com>
2022-05-16 13:08:27 -0500

    function getRandomPhraseAsArray

--- js/app.js
+++ js/app.js
@@ -1,14 +1,49 @@
 // click on the button reset to start the game 
 
 const btn_start =document.getElementsByClassName("btn__reset");
-console.log(btn_start[0]);
+
 
  btn_start[0].addEventListener("click",StartGame);
 
  function StartGame(){
-    console.log("button click");
+   
     const overlay = document.getElementById("overlay");
-    console.log(overlay);
- overlay.style.display ="none"
+    overlay.style.display ="none"
 
 }
+//Create a Phrases array that stores 5 strings 
+var phrases= ["Nermine","play","together","Haroun","Nice"];
+
+//create a fucntion to generate a random phrase from an array
+
+function getRandomPhraseAsArray(arr){
+
+var randomNumber = Math.floor(Math.random()*arr.length);
+var randomPhrase = [];
+
+
+if (randomNumber <= arr.length){
+    console.log(randomNumber);
+    return  randomPhrase = arr[randomNumber];
+ 
+
+}
+else {
+ 
+    return  randomPhrase = arr[5];
+
+
+}
+}
+console.log(getRandomPhraseAsArray(phrases));
+console.log(getRandomPhraseAsArray(phrases));
+console.log(getRandomPhraseAsArray(phrases));
+console.log(getRandomPhraseAsArray(phrases));
+
+
+
+
+
+
+
+
