f222459cc4e3515ddd26900035f55055edfc662f
amouna89 <95442375+amouna89@users.noreply.github.com>
2022-05-16 15:36:35 -0500

    addPhraseToDisplay

--- js/app.js
+++ js/app.js
@@ -35,11 +35,36 @@ else {
 
 }
 }
-console.log(getRandomPhraseAsArray(phrases));
-console.log(getRandomPhraseAsArray(phrases));
-console.log(getRandomPhraseAsArray(phrases));
-console.log(getRandomPhraseAsArray(phrases));
+var randomPhrase = getRandomPhraseAsArray(phrases);
+console.log(randomPhrase);
+//addPhraseToDisplay will loop the arr and display everyletter on the screen
 
+function addPhraseToDisplay(arr){
+   var item="";
+     
+for (let i=0 ; i < arr.length ; i++ ){
+
+console.log(arr[i]);
+
+    //create an li node
+    item = document.createElement("li"); 
+
+    // Create a text node:
+    var text = arr[i];
+    const textnode = document.createTextNode(text);
+
+   // Append the text node to the "li" node:
+     item.appendChild(textnode);
+
+     // Append the "li" node to the list:
+    document.getElementById("list").appendChild(item);
+      
+}
+
+}
+
+
+addPhraseToDisplay(randomPhrase);
 
 
 
