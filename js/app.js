8c7e7417eccc2a87237573168c558bf41170bee4
amouna89 <95442375+amouna89@users.noreply.github.com>
2022-06-06 16:59:01 -0500

    qwerty.addEventListener

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 const btn_start =document.getElementsByClassName("btn__reset");
 
+const  qwerty= document.getElementById("qwerty");
 
  btn_start[0].addEventListener("click",() => {
    
@@ -12,80 +13,102 @@ const btn_start =document.getElementsByClassName("btn__reset");
 
 
 //Create a Phrases array that stores 5 strings 
-var phrases= ["Nermine","play","together","Haroun","Nice"];
+var phrases= ["Nermine plays with Haroun","Haroun loves Nermine","Mom is cooking dinner","Dad is watching TV","Haroun is sleeping"];
 
 //create a fucntion to generate a random phrase from an array
+//adjust this function 
 
 function getRandomPhraseAsArray(arr){
 
 var randomNumber = Math.floor(Math.random()*arr.length);
 var randomPhrase = [];
 
-
-if (randomNumber <= arr.length){
-    console.log(randomNumber);
-    return  randomPhrase = arr[randomNumber];
- 
-
+if (randomNumber < 5){
+    randomPhrase =arr[randomNumber];
+    return  randomPhrase ;
 }
 else {
- 
-    return  randomPhrase = arr[5];
-
-
+    return arr[5];
 }
+    
+    
+
+ 
+    
+
+
 }
 var randomPhrase = getRandomPhraseAsArray(phrases);
 console.log(randomPhrase);
 //addPhraseToDisplay will loop the arr and display everyletter on the screen
 
 function addPhraseToDisplay(arr){
-   var item="";
-   
-     
-for (let i=0 ; i < arr.length ; i++ ){//begin for
+          
+    for (let i=0 ; i < arr.length ; i++ ){//begin for
 
-console.log(arr[i]);
-var list = document.querySelector("#phrase ul");
-    //create an li node
-    item = document.createElement("li"); 
+    var list = document.querySelector("#phrase ul");
+        //create an li node
+    var li = document.createElement("li"); 
+        // Create a text node:
+        li.textContent = arr[i];
+        if ( arr[i] == " "){
+            li.className ="space";
+        }
+        else{
+        li.className ="letter"
 
-    // Create a text node:
-    const textnode = document.createTextNode(arr[i]);
-
-   // Append the text node to the "li" node:
-     item.appendChild(textnode);
-
-     if (textnode == ""){
-        item.className ="space";
-        console.log("space");
+        }
         // Append the "li" node to the list:
-    list.appendChild(item);
+        list.appendChild(li);
+        
+        }//fin for
+console.log(li);
+        
 
-     }
-     else{
-    item.className ="letter"
-    console.log("letter");
-    // Append the "li" node to the list:
-    list.appendChild(item);
-     }
-    
-     
-    }//fin for
 
 }//fin de la fonction addPhraseToDisplay
 
 addPhraseToDisplay(randomPhrase);
 
-//var p = document.querySelector("p").textContent ="happy day"
 
 //checkLetter function
 
 function checkLetter(button) {
 
+    const buttonLetter =button.textContent;
+    const word = document.getElementsByClassName("letter");
+    
+    const match =null;
+
+    for(let i=0 ; i < word.length ; i++){
+
+        const li = word[i];
+        
+        if (buttonLetter === li.textContent){
+            li.className ="show";
+            match = buttonLetter;
+        }
+
+
+    }
+
+    return match;
+
+
 }
 
+qwerty.addEventListener("click", (event) => {
 
+    const button =event.target;
+
+    if (button.tagName === "BUTTON"){
+
+        console.log("a buttton");
+    }
+
+
+
+});
 
 
 
