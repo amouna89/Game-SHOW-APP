750b4775f108e1d371929711e6a11318c49ec688
amouna89 <95442375+amouna89@users.noreply.github.com>
2022-07-08 13:32:29 -0500

    list[0].style.display ="none";

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
 // click on the button reset to start the game 
 
 const btn_start =document.getElementsByClassName("btn__reset");
-
 const  qwerty= document.getElementById("qwerty");
-
 const overlay = document.getElementById("overlay");
+const hearts = document.getElementsByTagName("img");
+
+const phraseTOdisplay = document.getElementsByClassName("letter");
+const lettersPressed = document.getElementsByClassName("show");
+
+//Create a Phrases array that stores 5 strings 
+var phrases= ["nermine plays with haroun","haroun loves nermine","mom is cooking dinner","dad is playing with his phone","haroun is sleeping"];
+
+//missed is the counter to check how many wrong guesses the player made 
+var missed = 0;
+
+//the title to display in the ovelay
+const h2 = document.getElementsByClassName("title") ;
 
 
  btn_start[0].addEventListener("click",() => {
@@ -14,53 +25,53 @@ const overlay = document.getElementById("overlay");
 });
 
 
-//Create a Phrases array that stores 5 strings 
-var phrases= ["nermine plays with haroun","haroun loves nermine","mom is cooking dinner","dad is playing with his phone","haroun is sleeping"];
+
 
 //create a fucntion to generate a random phrase from an array
 //adjust this function 
 
 function getRandomPhraseAsArray(arr){
 
-var randomNumber = Math.floor(Math.random()*arr.length);
-var randomPhrase = [];
+    var randomNumber = Math.floor(Math.random()*arr.length);
+    var randomPhrase = [];
 
-if (randomNumber < 5){
-    randomPhrase =arr[randomNumber];
-    return  randomPhrase ;
-}
-else {
-    return arr[5];
-}
-    
+    if (randomNumber < 5){
+        randomPhrase =arr[randomNumber];
+        return  randomPhrase ;
+    }
+    else {
+        return arr[5];
+    }
+        
 
 }
 var randomPhrase = getRandomPhraseAsArray(phrases);
-console.log(randomPhrase);
+
+console.log (randomPhrase);
 //addPhraseToDisplay will loop the arr and display everyletter on the screen
 
 function addPhraseToDisplay(arr){
-          
-    for (let i=0 ; i < arr.length ; i++ ){//begin for
+            
+            for (let i=0 ; i < arr.length ; i++ ){//begin for
 
-    var list = document.querySelector("#phrase ul");
-        //create an li node
-    var li = document.createElement("li"); 
-        // Create a text node:
-        li.textContent = arr[i];
-        if ( arr[i] == " "){
-            li.className ="space";
-        }
-        else{
-        li.className ="letter"
+            var list = document.querySelector("#phrase ul");
+                //create an li node
+            var li = document.createElement("li"); 
+                // Create a text node:
+                li.textContent = arr[i];
+                if ( arr[i] == " "){
+                    li.className ="space";
+                }
+                else{
+                li.className ="letter"
 
-        }
-        // Append the "li" node to the list:
-        list.appendChild(li);
-        
-        }//fin for
-console.log(li);
-        
+                }
+                // Append the "li" node to the list:
+                list.appendChild(li);
+                
+                }//fin for
+            console.log(li);
+                
 
 
 }//fin de la fonction addPhraseToDisplay
@@ -69,115 +80,100 @@ addPhraseToDisplay(randomPhrase);
 
 
 //checkLetter function
-
 function checkLetter(button) {
 
-    const buttonLetter = button.textContent;
-    const word = document.getElementsByClassName("letter");
-    //Create a variable to store if a match is found and give it an initial value of null
-    var match = null;
+            const buttonLetter = button.textContent;
+            const word = document.getElementsByClassName("letter");
+            //Create a variable to store if a match is found and give it an initial value of null
+            var match = null;
 
-    for(let i=0 ; i < word.length ; i++){
+            for(let i=0 ; i < word.length ; i++){
 
-        const li = word[i];
-        
-        if (buttonLetter === li.textContent){
-            //this will overwrite the existing class
-            // li.className ="show";
-            // element.classList.add("mystyle"); 
-            // To add new classes, without overwriting existing values, add a space and the new classes:
-            // element.className += " class1 class2";
-
-            li.className += " show";      
-            match = buttonLetter;
+                const li = word[i];
                 
-        }
+                if (buttonLetter === li.textContent){
+                    //this will overwrite the existing class
+                    // li.className ="show";
+                    // To add new classes, without overwriting existing values, add a space and the new classes:
+                    // element.className += " class1 class2";
+
+                    li.className += " show";      
+                    match = buttonLetter;
+                        
+                }
 
 
-    }
+            }
 
-    return match;
+            return match;
 
 
 }
 
-//missed is the counter to check how many wrong guesses the player made 
 
-var missed = 0;
-
-//the title to display in the ovelay
-const h2 = document.getElementsByClassName("title") ;
-
-//add an event listener on the keyboard 
-
-qwerty.addEventListener ("click", (event) => {
-
-const button =event.target;
-
-const hearts = document.getElementsByTagName("img");
-
-if (button.tagName === "BUTTON"){
-    button.className ="chosen";
-    button.disabled = true;
-    var match =checkLetter(button);
-    
-    if (match == null) {
-        //the button pressed is not in correct so the guess is wrong,remove one of 
-        //the heart images and increment the missed counter 
-        
-        hearts[missed].src = "images/lostHeart.png";
-
-        if (missed <= 4 ){
-            missed ++;
-            
-        }
-        
-        
-    }//end if match == null
-   
-    }
-    checkWin(); 
-
-
-
-});
 
 //  checkWin function is used to verify if the game is over and if the user won or lost the game 
 
 function checkWin(){
 
-    const phraseTOdisplay = document.getElementsByClassName("letter");
-    const lettersPressed = document.getElementsByClassName("show");
-    
-
+    const list = document.getElementsByTagName("ul");
     if (phraseTOdisplay.length === lettersPressed.length){
-      
-        //the user guessed the phrase correctly 
-      //display the win overlay
+        
+            //the user guessed the phrase correctly 
+        //display the win overlay
+            overlay.className = "win";
+            h2[0].textContent ="You won great job";
+            btn_start[0].textContent ="TRY AGAIN";
+            overlay.style.display ="flex"; 
+            list[0].style.display ="none";
 
-      overlay.className = "win";
-      h2[0].textContent ="You won great job";
-      btn_start[0].textContent ="TRY AGAIN";
-      overlay.style.display ="flex";
 
-    }
+        }
 
-    if (missed >= 5 ){
-        //the user made 5 wrong guesses
-        //display the lose overlay
-        overlay.className = "lose";
-        h2[0].textContent ="better luck next time";
-        btn_start[0].textContent ="TRY AGAIN";
-        overlay.style.display ="flex";
+        if (missed >= 5 ){
 
-    }
+                //the user made 5 wrong guesses
+                //display the lose overlay
+                overlay.className = "lose";
+                h2[0].textContent ="better luck next time";
+                btn_start[0].textContent ="TRY AGAIN";
+                overlay.style.display ="flex";
+                list[0].style.display ="none";
+
+        }
 
 
 
 }
 
- checkWin(); 
-
+ //add an event listener on the keyboard 
+ qwerty.addEventListener ("click", (event) => {
+ 
+    const button =event.target;
+    
+    if (button.tagName === "BUTTON"){
+        button.className ="chosen";
+        button.disabled = true;
+        var match =checkLetter(button);
+        
+        if (match == null) {
+            //the button pressed is not in correct so the guess is wrong,remove one of 
+            //the heart images and increment the missed counter 
+            hearts[missed].src = "images/lostHeart.png";
+            if (missed <= 4 ){
+                missed ++;    
+            }
+            
+            
+        }//end if match == null
+        
+        }
+        checkWin(); 
+ 
+ 
+ 
+ });
+ 
 
 
 
